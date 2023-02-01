8b688ebb4c7a76e2ac47a7073df331925cb6ccaa
Greviansonula <greviansonula@gmail.com>
2023-02-01 08:30:28 +0300

    Configs

--- configs.py
+++ configs.py
@@ -1,22 +1,4 @@
 from pydantic import BaseSettings, Field
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# class Settings():
-#     db_name = os.getenv("NAME"),
-#     db_user = os.getenv("USER"), 
-#     db_user_password = os.getenv("PASSWORD"),
-#     db_host = os.getenv("HOST")
-#     smtp_server = os.getenv('SMTP_SERVER')
-#     email_port = os.getenv("EMAIL_PORT")
-#     sender_email = os.getenv("SENDER_EMAIL")
-#     email_user_password = os.getenv("EMAIL_USER_PASSWORD")
-    
-# def get_settings():
-#     return Settings()
-
 
 class Settings(BaseSettings):
     db_name: str = Field(..., env="NAME")
@@ -30,7 +12,7 @@ class Settings(BaseSettings):
     secret_key: str = Field(..., env="SECRET_KEY")
 
     class Config:
-        env_file = [".env", "../.env"]
+        env_file = [".env"]
 
 # @lru_cache
 def get_settings():
